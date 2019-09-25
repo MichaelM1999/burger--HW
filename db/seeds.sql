ad22551343452ad3a37b13d89cdc075141beda59
michael mariea <mariea.michael@gmail.com>
2019-09-24 18:26:09 -0700

    seeds and server

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO songs (burger_name, devoured)
+VALUES ("cheeseburger", 1);
+INSERT INTO songs (burger_name, devoured)
+VALUES ("hamburger", 1);
+INSERT INTO songs (burger_name, devoured)
+VALUES ("bacon burger", 1);
